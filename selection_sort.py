1251efb46ccaedb52fc026ae27b7d110feb9a183
Mauricio <vrosmeo@gmail.com>
2025-05-29 11:28:34 -0600

    Add function to find the index of the smallest element in an array

---
+++ selection_sort.py
@@ -0,0 +1,10 @@
+def find_smallerOne(arr):
+    smallest = arr[0]
+    smallestIndex = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallestIndex = i
+    return smallestIndex
+
+
